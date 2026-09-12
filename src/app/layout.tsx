798286222c30e184f2da83d457aeb61ed5e4beb3
Louis Volant <volantlouis@gmail.com>
2026-09-12 14:00:19 +0200

    Disable pinch and gesture zooming on mobile PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,8 @@ export const viewport: Viewport = {
   themeColor: '#000000',
   width: 'device-width',
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   viewportFit: 'cover',
 };
 
